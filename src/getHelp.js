50504f846ab4e6367385b1dffd52ad4a46f4ed63
enoshixi <enoshixi@gmail.com>
2017-10-24 12:48:14 -0500

    =Remove quote from id (#11)

--- src/getHelp.js
+++ src/getHelp.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function getHelp({help, attrs}) {
   if (help) {
-    return <span className="help-block" id={`${attrs.id}-tip'`}>{help}</span>
+    return <span className="help-block" id={`${attrs.id}-tip`}>{help}</span>
   }
 }
